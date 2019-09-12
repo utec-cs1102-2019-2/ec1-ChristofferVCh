71dfe4d455485d6a923551cd4e39a00941724e0d
Christoffer Vasquez Chirito <christoffer.vasquez@gmail.com>
2019-09-12 10:04:16 -0500

    Segundo

--- session4b/main.cpp
+++ session4b/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+using namespace std;
+void pedir_notas(float &a, float &b);
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    float pc1 = 0.0;
+    float bonus = 0.0;
+    pedir_notas(pc1, bonus);
+    cout << pc1 << " " << bonus<<endl;
+    float *puntero = &pc1;
+    *puntero=pc1+bonus;
+    cout<<puntero<<" "<<*puntero<<" "<<&puntero<<endl;
+
     return 0;
+}
+void pedir_notas(float &a, float &b){
+    cin>>a;
+    cin>>b;
 }
